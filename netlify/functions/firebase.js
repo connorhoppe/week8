7457c0beb87efa5cad141e6919e355eeaefac3b6
Connor Hoppe <connorhoppe@gmail.com>
2021-05-25 11:26:32 -0500

    Completed lab

--- netlify/functions/firebase.js
+++ netlify/functions/firebase.js
@@ -2,13 +2,13 @@ const firebase = require("firebase/app")
 require("firebase/firestore")
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCGd7OAVUT1u3Sf9uoSh_6l8iHg2qh_JJ4",
-  authDomain: "kiei-451-spring-2021.firebaseapp.com",
-  projectId: "kiei-451-spring-2021",
-  storageBucket: "kiei-451-spring-2021.appspot.com",
-  messagingSenderId: "902466001150",
-  appId: "1:902466001150:web:b5a42e97ab21026b706daf"
-} // replace
+  apiKey: "AIzaSyDmMKfdqTXwQhmeBSa0R8nFoioCCPj_Q9k",
+  authDomain: "kiei-451-17d8d.firebaseapp.com",
+  projectId: "kiei-451-17d8d",
+  storageBucket: "kiei-451-17d8d.appspot.com",
+  messagingSenderId: "811628484570",
+  appId: "1:811628484570:web:683d9a86f3a43d51a252e3"
+}
 
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig)
